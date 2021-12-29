a8005777dc7a92ed5fcd2b3f7231921ba83b487a
Timur <maddhighlights@gmail.com>
2021-12-29 20:44:18 +0600

    Добавление Celery и Debug Toolbar

--- forum/settings.py
+++ forum/settings.py
@@ -108,10 +108,6 @@ DEBUG_TOOLBAR_PANELS = [
 
 
 
-
-
-
-
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
@@ -183,3 +179,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "pythonpe13@gmail.com"
 EMAIL_HOST_PASSWORD = "KoreaFer12"
+
+
+
+
