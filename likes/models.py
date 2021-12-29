2b9b971a4fba37565a84d5d7a0de5030f6ad07ca
Timur <maddhighlights@gmail.com>
2021-12-29 20:45:55 +0600

    Возможность сделать лайк для поста и кто лайкнул пост

--- likes/models.py
+++ likes/models.py
@@ -9,3 +9,8 @@ class Like(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+
+
+
+
+    
